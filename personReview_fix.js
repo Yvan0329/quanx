b6ef35728b295daa4ebef190a041ddfb46460c2c
Yvan0329 <8574526@qq.com>
2025-08-09 11:15:28 +0800

    Update personReview_fix.js

--- personReview_fix.js
+++ personReview_fix.js
@@ -1,3 +1,20 @@
-// personReview_fix.js
-console.log("=== 命中 viewRcfsqrxx 重写 ===");
-$done({});
+/*
+ * 只修改 personReview
+ */
+
+let body = $response.body;
+
+try {
+  let obj = JSON.parse(body);
+
+  if (obj?.data) {
+    obj.data.personReview = 0; // 改成通过状态
+  }
+
+  body = JSON.stringify(obj);
+  console.log("=== 已将 personReview 改为 1 ===");
+} catch (e) {
+  console.log("修改 JSON 出错:", e);
+}
+
+$done({ body });
